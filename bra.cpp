f516de26dabf5a53a4b66a520527c0264c14d2de
Jos van den Oever <jos@vandenoever.info>
2015-06-23 08:17:15 +0200

    snapshot

--- bra.cpp
+++ bra.cpp
@@ -47,165 +47,31 @@ static const PQName p;
 static const SpanQName span;
 static const BrQName br;
 
-struct EndElement {
-};
-
 template <int depth>
-Writer<depth+1> operator<(Writer<depth>, QName) {
+Writer<depth+1> operator<(Writer<depth>, const QName&) {
     return Writer<depth+1>();
 }
 
 template <int depth>
-Writer<depth-1> operator>(Writer<depth>, QName) {
+Writer<depth-1> operator>(Writer<depth>, const QName&) {
     return Writer<depth-1>();
 }
-/*
-Writer operator<=(Writer a, QName) {
-    return a;
-}
-*/
 
 template <int depth>
-Writer<depth+1> operator<(Writer<depth>, const char*) {
-    return Writer<depth+1>();
+Writer<depth> operator<(Writer<depth>, const char*) {
+    return Writer<depth>();
 }
-/*
-template <int depth>
-Writer<depth+1> operator<(Writer<depth> a, EndElement) {
-    return Writer<depth+1>();
-}
-
-Writer operator>(Writer a, EndElement) {
-    return a;
-}
-EndElement operator!(EndElement) {
-    return EndElement();
-}
-
-EndElement end() {
-    return EndElement();
-}
-void crazy() {
-    Writer<> a;
-    a <html
-        <head
-          <title
-            <"hello world"
-          <end()
-        <end()
-        <body
-          <p
-            <"hello "
-            <span
-              <"world"
-            <end()
-          <end()
-          <br<end()
-        <end()
-      <end();
-}
-
-static const EndElement _;
-
-struct D {};
-void operator+=(D, std::initializer_list<int>) {}
-
-void crazy2() {
-    Writer a;
-    a <html
-        <head
-          <title
-            <"hello world"
-          <_
-        <_
-        <body
-          <p
-            <"hello "
-            <span
-              <"world"
-            <_
-          <_
-          <br<_
-        <0
-      <_;
-}
-
-void crazy3() {
-    Writer a;
-    a <html
-        <head
-          <title
-            <"hello world"
-        <body
-          <p
-            <"hello "
-            <span
-              <"world"
-          <br;
-}
-
-void crazy4() {
-    Writer a;
-    a <html
-        <head
-          <=title
-            <"hello world"
-        <=body
-          <p
-            <"hello "
-            <=span
-              <"world"
-          <=br;
-}
-
-void crazy5() {
-    Writer a;
-    a <html
-        <head
-          <title
-            <"hello world"
-          >title
-        >head
-        <body
-          <p
-            <"hello "
-            <span
-              <"world"
-            >span
-          >p
-          <br>br
-        >body
-      >html;
-}
-*/
-
 
 template <int depth>
 Writer<depth>
 makeParagraph(Writer<depth> w) {
-    return w;
-    /*
+    return w
     <p
       <"hello "
       <span
         <"world"
       >span
     >p;
-    */
-}
-
-
-Writer<>
-makeParagraph2(Writer<> w) {
-    return w;
-    /*
-    <p
-      <"hello "
-      <span
-        <"world"
-      >span
-    >p;
-    */
 }
 
 class Functor {
@@ -213,46 +79,30 @@ public:
     template<int depth>
     Writer<depth> operator()(Writer<depth> w) {return w;}
 };
-/*
+
 template<typename F, int depth>
 auto operator<(Writer<depth> w, F f) -> decltype(f(w)) {
     return f(w);
 }
-*/
 
 template<int depth>
 Writer<depth> operator<(Writer<depth> w, Writer<depth> (*f)(Writer<depth>)) {
-    return w;// f(w);
-}
-
-/*
-template<typename F, int depth>
-Writer<depth> operator<(Writer<depth> w, F f) {
     return f(w);
 }
 
-
-template<int depth>
-Writer<depth> operator<(Writer<depth> w, int f) {
-    return f(w);
-}
-*/
-
-void crazy6() {
+void test() {
     Functor f;
     Writer<> a;
-    auto d = a <html
+    a <html
         <head
           <title
             <"hello world"
           >title
         >head
         <body
-          <makeParagraph
-          <br;//>br;
-
-    d
-          //<f
+          <(makeParagraph)
+          <br>br
+          <f
           <br>br
         >body
       >html;
