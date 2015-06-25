972d66b055035a625eacbcf74ee03315d309f955
Jos van den Oever <jos@vandenoever.info>
2015-06-25 18:44:33 +0200

    Add another test directory.

---
+++ tests/tests.pri
@@ -0,0 +1,6 @@
+TEMPLATE = app
+CONFIG += testcase
+QT += testlib
+QT -= gui
+INCLUDEPATH += ../..
+QMAKE_CXXFLAGS = -std=c++11
---
+++ tests/xpath/testxpath.cpp
@@ -0,0 +1,151 @@
+#include <QObject>
+#include <QTest>
+#include <XmlWriter.h>
+#include <LiteralXml.h>
+class TestSerializer : public QObject
+{
+    Q_OBJECT
+private Q_SLOTS: // tests
+    void writeElement();
+    void writeElements();
+    void writeCharacters();
+    void writeAttribute();
+    void writeAttributes();
+    void writeWithFunction();
+    void writeWithFunctor();
+    void writeListWithFunctor();
+};
+
+namespace {
+
+const QString htmlns = QStringLiteral("http://www.w3.org/1999/xhtml");
+const QString htmlTag = QStringLiteral("html");
+const QString headTag = QStringLiteral("head");
+const QString titleTag = QStringLiteral("title");
+const QString idTag = QStringLiteral("id");
+const QString classTag = QStringLiteral("class");
+const QString empty;
+XmlTag<&htmlns, &htmlTag, false, true> html;
+XmlTag<&htmlns, &headTag, false, true> head;
+XmlTag<&htmlns, &titleTag, false, true> title;
+XmlTag<&empty, &idTag, true, false> id;
+XmlTag<&empty, &classTag, true, false> class_;
+}
+
+void
+TestSerializer::writeElement() {
+    QString r;
+    QXmlStreamWriter stream(&r);
+    XmlWriter<>(stream)
+    <html>html;
+    QCOMPARE(r, QString("<n1:html xmlns:n1=\"http://www.w3.org/1999/xhtml\"/>"));
+}
+
+void
+TestSerializer::writeElements() {
+    QString r;
+    QXmlStreamWriter stream(&r);
+    XmlWriter<>(stream)
+    <html
+      <head>head
+    >html;
+    QCOMPARE(r, QString("<n1:html xmlns:n1=\"http://www.w3.org/1999/xhtml\"><n1:head/></n1:html>"));
+}
+void
+TestSerializer::writeCharacters() {
+    QString r;
+    QXmlStreamWriter stream(&r);
+    XmlWriter<>(stream)
+    <html
+      <head
+        <title
+          <"Hello world"
+        >title
+      >head
+    >html;
+    QCOMPARE(r, QString("<n1:html xmlns:n1=\"http://www.w3.org/1999/xhtml\"><n1:head><n1:title>Hello world</n1:title></n1:head></n1:html>"));
+}
+void
+TestSerializer::writeAttribute() {
+    QString r;
+    QXmlStreamWriter stream(&r);
+    XmlWriter<>(stream)
+    <html
+      <head({id="v1.1"})
+      >head
+    >html;
+    QCOMPARE(r, QString("<n1:html xmlns:n1=\"http://www.w3.org/1999/xhtml\"><n1:head id=\"v1.1\"/></n1:html>"));
+}
+void
+TestSerializer::writeAttributes() {
+    QString r;
+    QXmlStreamWriter stream(&r);
+    XmlWriter<>(stream)
+    <html
+      <head({id="v1.1",class_="main"})
+      >head
+    >html;
+    QCOMPARE(r, QString("<n1:html xmlns:n1=\"http://www.w3.org/1999/xhtml\"><n1:head id=\"v1.1\" class=\"main\"/></n1:html>"));
+}
+template <typename Base, typename Tag>
+XmlWriter<Base,Tag>
+makeHead(XmlWriter<Base,Tag> w) {
+    return w
+    <head>head;
+}
+void
+TestSerializer::writeWithFunction() {
+    QString r;
+    QXmlStreamWriter stream(&r);
+    XmlWriter<>(stream)
+    <html
+      <makeHead
+    >html;
+    QCOMPARE(r, QString("<n1:html xmlns:n1=\"http://www.w3.org/1999/xhtml\"><n1:head/></n1:html>"));
+}
+class Functor {
+public:
+    const QString text;
+    Functor(const QString& text_) :text(text_) {}
+    template <typename Base, typename Tag>
+    XmlWriter<Base,Tag> operator()(XmlWriter<Base,Tag> w) {
+        return w <head<text>head;
+    }
+};
+void
+TestSerializer::writeWithFunctor() {
+    Functor f("HELLO");
+    QString r;
+    QXmlStreamWriter stream(&r);
+    XmlWriter<>(stream)
+    <html
+      <f
+    >html;
+    QCOMPARE(r, QString("<n1:html xmlns:n1=\"http://www.w3.org/1999/xhtml\"><n1:head>HELLO</n1:head></n1:html>"));
+}
+class ListFunctor {
+public:
+    const std::list<QString> texts;
+    ListFunctor(const std::list<QString>& texts_) :texts(texts_) {}
+    template <typename Base, typename Tag>
+    XmlWriter<Base,Tag> operator()(XmlWriter<Base,Tag> w) {
+        for (const QString& t: texts) {
+            w <head <t >head;
+        }
+        return w;
+    }
+};
+void
+TestSerializer::writeListWithFunctor() {
+    ListFunctor f({"A","B"});
+    QString r;
+    QXmlStreamWriter stream(&r);
+    XmlWriter<>(stream)
+    <html
+      <f
+    >html;
+    QCOMPARE(r, QString("<n1:html xmlns:n1=\"http://www.w3.org/1999/xhtml\"><n1:head>A</n1:head><n1:head>B</n1:head></n1:html>"));
+}
+
+QTEST_APPLESS_MAIN(TestSerializer)
+#include "testxpath.moc"
